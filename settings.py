52f459207302ca55446204751544b6c3c027ebea
Zach Williams <hey@zachwill.com>
2011-08-28 13:01:39 -0700

    More DotCloud settings

--- settings.py
+++ settings.py
@@ -22,6 +22,7 @@ DATABASES = {
 # This should work both locally and on DotCloud.
 if os.path.exists('/home/dotcloud'):
     os.symlink('/home/dotcloud/test.db', 'test.db')
+    DATABASE_NAME = '/home/dotcloud/test.db'
     DB_PATH = '/home/dotcloud/test.db'
 else:
     DB_PATH = 'test.db'
