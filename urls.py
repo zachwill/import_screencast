1d713edc623f56f2a99ba4a2508fae1225aaee1e
Zach Williams <hey@zachwill.com>
2011-08-27 22:26:05 -0700

    Minor changes to URL structure

--- urls.py
+++ urls.py
@@ -18,8 +18,6 @@ urlpatterns = patterns('screencasts.views',
 
 
 urlpatterns += patterns('',
-    # Uncomment the admin/doc line below to enable admin documentation:
-    url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
-    # Uncomment the next line to enable the admin:
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
 )
